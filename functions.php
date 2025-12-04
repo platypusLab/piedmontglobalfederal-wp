6a75d57eee3df768fdef165f47272d672683f02f
Godwill Barasa <33608094+godwillcodes@users.noreply.github.com>
2025-12-04 15:00:51 +0300

    Implement functionality to disable comments across all post types, including removal from admin menu, dashboard, and front-end. Also deregister the comment-reply script and hide existing comments.

--- functions.php
+++ functions.php
@@ -152,10 +152,6 @@ function pgf_scripts() {
 	// Enqueue Alpine.js
 	wp_enqueue_script( 'alpinejs', '//unpkg.com/alpinejs', array(), null, true );
 	wp_script_add_data( 'alpinejs', 'defer', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'pgf_scripts' );
 
@@ -185,4 +181,69 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+/**
+ * Disable comments functionality.
+ */
+// Remove comment support from all post types
+function pgf_disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ( $post_types as $post_type ) {
+		if ( post_type_supports( $post_type, 'comments' ) ) {
+			remove_post_type_support( $post_type, 'comments' );
+			remove_post_type_support( $post_type, 'trackbacks' );
+		}
+	}
+}
+add_action( 'admin_init', 'pgf_disable_comments_post_types_support' );
+
+// Close comments on the front-end
+function pgf_disable_comments_status() {
+	return false;
+}
+add_filter( 'comments_open', 'pgf_disable_comments_status', 20, 2 );
+add_filter( 'pings_open', 'pgf_disable_comments_status', 20, 2 );
+
+// Hide existing comments
+function pgf_disable_comments_hide_existing_comments( $comments ) {
+	$comments = array();
+	return $comments;
+}
+add_filter( 'comments_array', 'pgf_disable_comments_hide_existing_comments', 10, 2 );
+
+// Remove comments page from menu
+function pgf_disable_comments_admin_menu() {
+	remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'pgf_disable_comments_admin_menu' );
+
+// Redirect any user trying to access comments page
+function pgf_disable_comments_admin_menu_redirect() {
+	global $pagenow;
+	if ( $pagenow === 'edit-comments.php' ) {
+		wp_redirect( admin_url() );
+		exit;
+	}
+}
+add_action( 'admin_init', 'pgf_disable_comments_admin_menu_redirect' );
+
+// Remove comments metabox from dashboard
+function pgf_disable_comments_dashboard() {
+	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+}
+add_action( 'admin_init', 'pgf_disable_comments_dashboard' );
+
+// Remove comments links from admin bar
+function pgf_disable_comments_admin_bar() {
+	if ( is_admin_bar_showing() ) {
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+	}
+}
+add_action( 'init', 'pgf_disable_comments_admin_bar' );
+
+// Remove comment-reply script from being enqueued
+function pgf_disable_comment_reply_script() {
+	wp_deregister_script( 'comment-reply' );
+}
+add_action( 'init', 'pgf_disable_comment_reply_script' );
 
